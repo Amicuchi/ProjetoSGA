77b0b82617a071d0f5a1fafb493db51415b5dd6b
Anderson Amicuchi <andersonamicuchi@gmail.com>
2024-05-18 17:44:05 -0300

    diversas alterações iniciais

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -31,11 +31,7 @@ export const Th = styled.th`
 export const Td = styled.td`
     padding-top: 15px;
     text-align: ${(props) => (props.alignCenter ? "center" : "start")};
-    width: ${(props) => (props.width ? props.width : "auto")};
-
-    @media (max-width: 500px){
-        ${(props) => props.onlyWeb && "display: none"}
-    }
+    width: ${(props) => (props.width ? props.width : "auto")}
 `;
 
 const Grid = ({ users }) => {
